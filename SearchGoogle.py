fdde232aef2ca1515c2524bf96b56c19b7807c04
rezalution <rezalution786@gmail.com>
2014-09-03 13:59:14 -0700

    applic

--- SearchGoogle.py
+++ SearchGoogle.py
@@ -178,7 +178,7 @@ class MainHandler(webapp2.RequestHandler)
 
   googleSearch(userId, startTimeParam, startDate, endTime, endDate)
   
-  app = webapp2.WSGIApplication([
+ app = webapp2.WSGIApplication([
 	('/', MainHandler),
 	#('/google', googleSearch)
 ], debug=True)
